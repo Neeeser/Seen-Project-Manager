a54658263fae083499f0e27d127c18c0607b32f9
Neeeser <andrew.neeser@me.com>
2023-06-30 14:21:45 -0400

    Submit Updates

--- Gui.py
+++ Gui.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 from Database import Database
+from Project import Project
 
 # All the stuff inside your window.
 alayout = [[sg.Text('Some text on Row 1')],
@@ -33,8 +34,8 @@ class DesktopGui:
         ### Sets up the layout of the page
         self.layout = [[sg.Button('Submit'), sg.Button('Exit')],
                        [sg.Combo(values=tuple(self.db.projects.keys()), default_value='None', readonly=False,
-                                 k='-COMBO-', enable_events=True)
-                           , sg.Text("project.owner"), sg.Text("project.project_name")]]
+                                 k='-COMBO-', enable_events=True, size=30)
+                           , sg.Text("Project:", visible=False), sg.Text("Owner:", visible=False)]]
         self.setup_reports()
 
         sg.theme('BlueMono')  # Add a touch of color
@@ -49,29 +50,41 @@ class DesktopGui:
                 break
 
             if self.event == 'Submit':
-                self.button_go()
-                # self.layout[3][2].update(visible=not self.layout[3][2].visible)
+                self.submit_report()
 
             if self.event == '-COMBO-':
                 self.displayed_project = self.db.projects[self.values['-COMBO-']]
-
+                self.load_project_into_layout(self.displayed_project)
         self.window.close()
 
     def setup_reports(self):
         temp_layout = [[], []]
         for i in range(self.max_reports):
-            temp_layout[0].append(sg.Text("NOT DEFINED", visible=False, pad=(50, 0)))
+            temp_layout[0].append(sg.Text("NOT DEFINED", visible=False, pad=(1, 0), expand_x=True))
             temp_layout[1].append(
-                sg.Multiline('Insert report here', size=(10, 5), expand_x=True, expand_y=True, k='report' + str(i),
-                             visible=True))
+                sg.Multiline('', size=(10, 10), expand_x=True, expand_y=True, k='report' + str(i),
+                             visible=False))
         self.layout.insert(self.reports_row, temp_layout)
 
-    def button_go(self):
-        sg.popup('Go button clicked', 'Input value:', self.values['report0'])
+    def submit_report(self):
+        if self.displayed_project is None:
+            return False
+        for i in range(len(self.displayed_project.reports_to)):
+            if self.values['report' + str(i)] != '':
+                self.displayed_project.add_report(self.displayed_project.owner, self.displayed_project.reports_to[i],
+                                                  self.values['report' + str(i)])
 
-    def load_project_into_layout(self):
-        for project in self.db.projects:
-            project = self.db.projects[project]
-            
+    def load_project_into_layout(self, project: Project):
+        self.layout[1][1].update(value="Project: " + project.project_name, visible=True)
+        self.layout[1][2].update(value="Owner: " + project.owner, visible=True)
+
+        for i in range(len(project.reports_to)):
+            self.layout[self.reports_row][0][i].update(visible=True, value=project.reports_to[i])
+            self.layout[self.reports_row][1][i].update(visible=True)
+
+        for i in range(len(project.reports_to), self.max_reports):
+            self.layout[self.reports_row][0][i].update(visible=False)
+            self.layout[self.reports_row][1][i].update(visible=False)
+
 
 DesktopGui().run()
