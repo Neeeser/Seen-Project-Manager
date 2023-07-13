1f2e6ff0e002cf9b66f0bf4a4450f54799b6baf7
Neeeser <andrew.neeser@me.com>
2023-07-13 16:13:02 -0400

    Compiler for Windows

--- compiler.py
+++ compiler.py
@@ -1,5 +1,6 @@
 import PyInstaller.__main__
 from sys import platform
+
 if platform == "linux" or platform == "linux2":
     # linux
     print()
@@ -20,4 +21,14 @@ elif platform == "darwin":
 
 elif platform == "win32":
     # Windows...
-    print()
+    PyInstaller.__main__.run([
+        "Gui.py",
+        '--onefile',
+        "--add-data=seenworkflow_private_key.json;\\",
+        "--add-data=manulife.ico;\\",
+        '--windowed',
+        '--icon=manulife.ico',
+        '--name=Seen',
+        '-y'
+
+    ])
